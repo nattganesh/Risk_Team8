c7b8682dd5dc2c5d550f6f91961b514d3d2f3f0c
Natheepan Ganeshamoorthy <nattganesh@outlook.com>
2019-01-27 23:07:04 -0500

    Creation of input txt file, and .java class Run to parse input txt file (first Version)

--- src/com/risk/run/Run.java
+++ src/com/risk/run/Run.java
@@ -5,6 +5,15 @@
  */
 package com.risk.run;
 
+import com.risk.map.Continent;
+import com.risk.map.Country;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Natheepan
@@ -13,5 +22,51 @@ public class Run {
 
     public static void main(String[] args)
     {
+        ArrayList<Continent> continents = new ArrayList<>();
+        try
+        {
+            Scanner input = new Scanner(new File("src/com/risk/run/inputtext/input.txt"));
+            int totalNumberOfCountries = 0;
+            while (input.hasNextLine())
+            {
+                String text = input.nextLine();
+                String nameOfContinent = text.substring(0, text.indexOf(","));
+                String nameOfCountry = text.substring(text.indexOf(",") + 1, text.length());
+                System.out.println(nameOfContinent + " " + nameOfCountry);
+
+                boolean continentExists = false;
+
+                for (Continent cont : continents)
+                {
+                    if (cont.getName().equalsIgnoreCase(nameOfContinent))
+                    {
+                        continentExists = true;
+                        cont.getCountries().add(new Country(nameOfCountry, false, null));
+                    }
+                }
+
+                if (!continentExists)
+                {
+                    continents.add(new Continent(nameOfContinent, 10));
+                    continents.get(continents.size() - 1).getCountries().add(new Country(nameOfCountry, false, null));
+                }
+
+                totalNumberOfCountries++;
+            }
+            System.out.println(totalNumberOfCountries);
+        }
+        catch (FileNotFoundException ex)
+        {
+            Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        for (Continent cont : continents)
+        {
+            for (Country country : cont.getCountries())
+            {
+                System.out.println(cont.getName() + " " + country.getName() + " " + country.isIsOccupied());
+            }
+        }
+
     }
 }
