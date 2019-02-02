416a0441b673751e0948d193bf3f19effbc1f312
Dong Jae Kim <djaekim086@gmail.com>
2019-02-02 22:23:41 +0000

    Neighbours are fixed for fixed map for each MAPObject

---
+++ src/com/risk/MapUtill/Neighbour.java
@@ -0,0 +1,10 @@
+package mapUtil;
+
+import java.util.ArrayList;
+
+public class Neighbour {
+	private ArrayList<Integer> neighbour = new ArrayList <Integer> ();
+	public void Neighbour() {
+		
+	}
+}
