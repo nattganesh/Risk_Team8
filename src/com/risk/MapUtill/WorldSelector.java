e9dfaecf64b563ff2291e55680c798c58127938b
Dong Jae Kim <djaekim086@gmail.com>
2019-02-02 22:22:27 +0000

    WorldSelector creates eventListener for MAPObject

---
+++ src/com/risk/MapUtill/WorldSelector.java
@@ -0,0 +1,28 @@
+package mapUtil;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class WorldSelector extends MouseAdapter {
+	private World world;
+	
+	public WorldSelector (World w) {
+		world = w;
+	}
+	public void mousePressed(MouseEvent e) {
+		Point p = e.getPoint();		
+		for (MAPObject obj : world.returnWorld()) {
+			
+			
+			if (obj.contains(p)) {
+				obj.addArmy(Color.yellow);
+				world.repaint();
+				break;
+			}
+			
+		}
+	}
+	
+}
