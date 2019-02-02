e2c7c76b4da5bdccef0f78dc4f018f667d10db52
Natheepan Ganeshamoorthy <nattganesh@outlook.com>
2019-02-01 20:16:20 -0500

    Seperate run.java main method into smaller methods to reduce cludder

--- src/com/risk/run/Run.java
+++ src/com/risk/run/Run.java
@@ -27,165 +27,249 @@ public class Run {
     public static ArrayList<Continent> continents = new ArrayList<>();
     public static ArrayList<Country> countries = new ArrayList<>();
     public static ArrayList<Player> players = new ArrayList<>();
+    private static String text = "";
+
+    public static void setPlayers(Scanner input) throws CountLimitException, CannotFindException
+    {
+        if (input.hasNextInt())
+        {
+            int numberOfPlayers = input.nextInt();
+            if (numberOfPlayers < 2 || numberOfPlayers > 6)
+            {
+                CountLimitException ex = new CountLimitException("Players", numberOfPlayers, 2, 6);
+                throw ex;
+            }
+            else
+            {
+                int a = 0;
+                input.nextLine();
+                while (a != numberOfPlayers)
+                {
+                    players.add(new Player(input.nextLine()));
+                    a++;
+                }
+            }
+        }
+        else
+        {
+            CannotFindException ex = new CannotFindException("The first line of Input Text Document is reserved for the number of players playing the game. This is undefined. This number should be between 2 and 6. Please resolve this issue.");
+            throw ex;
+        }
+    }
+
+    public static void setCountriesInContinents(Scanner input) throws CannotFindException, DuplicatesException
+    {
+        if (input.hasNextLine())
+        {
+            text = input.nextLine();
+            if (text.equalsIgnoreCase("SET COUNTRIES IN CONTINENTS") && input.hasNextLine())
+            {
+                text = input.nextLine();
+                int b = 1;
+                while (input.hasNextLine() && b <= Country.MAX_NUMBER_OF_COUNTRIES)
+                {
+                    b++;
+                    String nameOfContinent = text.substring(0, text.indexOf(","));
+                    String nameOfCountry = text.substring(text.indexOf(",") + 1, text.length());
+                    boolean countryExists = false;
+                    for (Country country : countries)
+                    {
+                        if (country.getName().equalsIgnoreCase(nameOfCountry))
+                        {
+                            countryExists = true;
+                        }
+                    }
+                    if (!countryExists)
+                    {
+                        boolean continentExists = false;
+
+                        Country c = new Country(nameOfCountry, nameOfContinent);
+                        countries.add(c);
+
+                        for (Continent cont : continents)
+                        {
+                            if (cont.getName().equalsIgnoreCase(nameOfContinent))
+                            {
+                                continentExists = true;
+                                cont.getCountries().add(c);
+                            }
+                        }
+
+                        if (!continentExists)
+                        {
+                            continents.add(new Continent(nameOfContinent, 10));
+                            continents.get(continents.size() - 1).getCountries().add(c);
+                        }
+                    }
+                    else
+                    {
+                        DuplicatesException ex1 = new DuplicatesException("Country: " + nameOfCountry);
+                        throw ex1;
+                    }
+                    text = input.nextLine();
+                }
+            }
+            else
+            {
+                CannotFindException ex = new CannotFindException("The tag 'SET COUNTRIES IN CONTINENTS' is not set. Please follow the Input Text Format Please resolve this issue.");
+                throw ex;
+            }
+        }
+        else
+        {
+            CannotFindException ex = new CannotFindException("Please follow the Input Text Format Please resolve this issue.");
+            throw ex;
+        }
+    }
+
+    public static void setNeighboringCountries(Scanner input) throws CannotFindException
+    {
+        if (input.hasNextLine() && text.equalsIgnoreCase("SET NEIGHBORS"))
+        {
+            while (input.hasNextLine())
+            {
+                text = input.nextLine();
+                String nameOfCountry1 = text.substring(0, text.indexOf(","));
+                String nameOfCountry2 = text.substring(text.indexOf(",") + 1, text.length());
+
+                for (Country c : countries)
+                {
+                    if (c.getName().equalsIgnoreCase(nameOfCountry1))
+                    {
+                        for (Country c2 : countries)
+                        {
+                            if (c2.getName().equalsIgnoreCase(nameOfCountry2))
+                            {
+                                c.getConnectedCountries().add(c2);
+                                c2.getConnectedCountries().add(c);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            CannotFindException ex = new CannotFindException("The tag 'SET NEIGHBORS' is not set. Please follow the Input Text Format Please resolve this issue.");
+            throw ex;
+        }
+    }
+
+    public static void continentChecks() throws CannotFindException, CountLimitException
+    {
+        for (Continent cont : continents)
+        {
+            int count = cont.getCountries().size();
+            int maxCount = 0;
+
+            switch (cont.getName())
+            {
+                case "North America":
+                    maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_NORTH_AMERICA;
+                    break;
+                case "South America":
+                    maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_SOUTH_AMERICA;
+                    break;
+                case "Europe":
+                    maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_EUROPE;
+                    break;
+                case "Asia":
+                    maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_ASIA;
+                    break;
+                case "Africa":
+                    maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_AFRICA;
+                    break;
+                case "Australia":
+                    maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_AUSTRALIA;
+                    break;
+                default:
+                    CannotFindException ex2 = new CannotFindException(cont.getName() + " is not predefined. Size of continent is not known. Please resolve this issue.");
+                    throw ex2;
+            }
+
+            if (maxCount != count)
+            {
+                CountLimitException ex3 = new CountLimitException(cont.getName(), count, maxCount);
+                throw ex3;
+            }
+        }
+    }
+
+    public static void assignCountriesToPlayers()
+    {
+        Player.setStartingPoints(players.size());
+
+        boolean[] countryOccupied = new boolean[Country.MAX_NUMBER_OF_COUNTRIES];
+        int i = 0;
+        while (i < Country.MAX_NUMBER_OF_COUNTRIES)
+        {
+            for (Player p : players)
+            {
+                int random = (int) (Math.random() * Country.MAX_NUMBER_OF_COUNTRIES);
+                while (countryOccupied[random])
+                {
+                    random = (int) (Math.random() * Country.MAX_NUMBER_OF_COUNTRIES);
+                }
+                if (!countryOccupied[random])
+                {
+                    countries.get(random).setRuler(p);
+                    countries.get(random).setIsOccupied(true);
+                    i++;
+                    countries.get(random).setArmyCount(1);
+
+                    countryOccupied[random] = true;
+                    p.getOccupiedCountries().add(countries.get(random));
+                }
+                if (i >= Country.MAX_NUMBER_OF_COUNTRIES)
+                {
+                    break;
+                }
+            }
+        }
+
+        boolean[] armiesRemaining = new boolean[players.size()];
+        boolean done = false;
+        while (!done)
+        {
+            for (int ii = 0; ii < players.size(); ii++)
+            {
+                if (players.get(ii).playersLeft() > 0)
+                {
+                    int random = (int) (Math.random() * players.get(ii).getOccupiedCountries().size());
+                    players.get(ii).getOccupiedCountries().get(random).setArmyCount(1);
+                }
+                else
+                {
+                    armiesRemaining[ii] = true;
+                }
+            }
+            int countP = 0;
+            for (boolean d : armiesRemaining)
+            {
+                if (d)
+                {
+                    countP++;
+                }
+            }
+            if (countP == players.size())
+            {
+                done = true;
+            }
+        }
+    }
 
     public static void main(String[] args)
     {
-        String text = "";
+
         try
         {
             Scanner input = new Scanner(new File("src/com/risk/run/inputtext/input.txt"));
-            if (input.hasNextInt())
-            {
-                int numberOfPlayers = input.nextInt();
-                if (numberOfPlayers < 2 || numberOfPlayers > 6)
-                {
-                    CountLimitException ex = new CountLimitException("Players", numberOfPlayers, 2, 6);
-                    throw ex;
-                }
-                else
-                {
-                    int a = 0;
-                    input.nextLine();
-                    while (a != numberOfPlayers)
-                    {
-                        players.add(new Player(input.nextLine()));
-                        a++;
-                    }
-                }
-            }
-            else
-            {
-                CannotFindException ex = new CannotFindException("The first line of Input Text Document is reserved for the number of players playing the game. This is undefined. This number should be between 2 and 6. Please resolve this issue.");
-                throw ex;
-            }
-            if (input.hasNextLine())
-            {
-                text = input.nextLine();
-                if (text.equalsIgnoreCase("SET COUNTRIES IN CONTINENTS") && input.hasNextLine())
-                {
-                    text = input.nextLine();
-                    int b = 1;
-                    while (input.hasNextLine() && b <= Country.MAX_NUMBER_OF_COUNTRIES)
-                    {
-                        b++;
-                        String nameOfContinent = text.substring(0, text.indexOf(","));
-                        String nameOfCountry = text.substring(text.indexOf(",") + 1, text.length());
-                        boolean countryExists = false;
-                        for (Country country : countries)
-                        {
-                            if (country.getName().equalsIgnoreCase(nameOfCountry))
-                            {
-                                countryExists = true;
-                            }
-                        }
-                        if (!countryExists)
-                        {
-                            boolean continentExists = false;
-
-                            Country c = new Country(nameOfCountry, nameOfContinent);
-                            countries.add(c);
-
-                            for (Continent cont : continents)
-                            {
-                                if (cont.getName().equalsIgnoreCase(nameOfContinent))
-                                {
-                                    continentExists = true;
-                                    cont.getCountries().add(c);
-                                }
-                            }
-
-                            if (!continentExists)
-                            {
-                                continents.add(new Continent(nameOfContinent, 10));
-                                continents.get(continents.size() - 1).getCountries().add(c);
-                            }
-                        }
-                        else
-                        {
-                            DuplicatesException ex1 = new DuplicatesException("Country: " + nameOfCountry);
-                            throw ex1;
-                        }
-                        text = input.nextLine();
-                    }
-                }
-                else
-                {
-                    CannotFindException ex = new CannotFindException("The tag 'SET COUNTRIES IN CONTINENTS' is not set. Please follow the Input Text Format Please resolve this issue.");
-                    throw ex;
-                }
-
-            }
-            else
-            {
-                CannotFindException ex = new CannotFindException("Please follow the Input Text Format Please resolve this issue.");
-                    throw ex;
-            }
-            if (input.hasNextLine() && text.equalsIgnoreCase("SET NEIGHBORS"))
-            {
-                while (input.hasNextLine())
-                {
-                    text = input.nextLine();
-                    String nameOfCountry1 = text.substring(0, text.indexOf(","));
-                    String nameOfCountry2 = text.substring(text.indexOf(",") + 1, text.length());
-
-                    for (Country c : countries)
-                    {
-                        if (c.getName().equalsIgnoreCase(nameOfCountry1))
-                        {
-                            for (Country c2 : countries)
-                            {
-                                if (c2.getName().equalsIgnoreCase(nameOfCountry2))
-                                {
-                                    c.getConnectedCountries().add(c2);
-                                    c2.getConnectedCountries().add(c);
-                                }
-                            }
-                        }
-                    }
-                }
-
-            }
-            else
-            {
-            CannotFindException ex = new CannotFindException("The tag 'SET NEIGHBORS' is not set. Please follow the Input Text Format Please resolve this issue.");
-                    throw ex;
-            }
-            for (Continent cont : continents)
-            {
-                int count = cont.getCountries().size();
-                int maxCount = 0;
-
-                switch (cont.getName())
-                {
-                    case "North America":
-                        maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_NORTH_AMERICA;
-                        break;
-                    case "South America":
-                        maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_SOUTH_AMERICA;
-                        break;
-                    case "Europe":
-                        maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_EUROPE;
-                        break;
-                    case "Asia":
-                        maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_ASIA;
-                        break;
-                    case "Africa":
-                        maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_AFRICA;
-                        break;
-                    case "Australia":
-                        maxCount = Continent.MAX_NUMBER_OF_COUNTRIES_IN_AUSTRALIA;
-                        break;
-                    default:
-                        CannotFindException ex2 = new CannotFindException(cont.getName() + " is not predefined. Size of continent is not known. Please resolve this issue.");
-                        throw ex2;
-                }
-
-                if (maxCount != count)
-                {
-                    CountLimitException ex3 = new CountLimitException(cont.getName(), count, maxCount);
-                    throw ex3;
-                }
-            }
+            setPlayers(input);
+            setCountriesInContinents(input);
+            setNeighboringCountries(input);
+            continentChecks();
+            assignCountriesToPlayers();
 
 // FOR DEBUGGING PURPOSE -------------------------------------------------------------------------------------
 //            int y = 0;
@@ -208,67 +292,7 @@ public class Run {
 //            players.add(new Player("Yellow"));
 //            players.add(new Player("Orange"));
 //            players.add(new Player("Purple"));
-            Player.setStartingPoints(players.size());
-
-            boolean[] countryOccupied = new boolean[Country.MAX_NUMBER_OF_COUNTRIES];
-            int i = 0;
-            while (i < Country.MAX_NUMBER_OF_COUNTRIES)
-            {
-                for (Player p : players)
-                {
-                    int random = (int) (Math.random() * Country.MAX_NUMBER_OF_COUNTRIES);
-                    while (countryOccupied[random])
-                    {
-                        random = (int) (Math.random() * Country.MAX_NUMBER_OF_COUNTRIES);
-                    }
-                    if (!countryOccupied[random])
-                    {
-                        countries.get(random).setRuler(p);
-                        countries.get(random).setIsOccupied(true);
-                        i++;
-                        countries.get(random).setArmyCount(1);
-
-                        countryOccupied[random] = true;
-                        p.getOccupiedCountries().add(countries.get(random));
-                    }
-                    if (i >= Country.MAX_NUMBER_OF_COUNTRIES)
-                    {
-                        break;
-                    }
-                }
-            }
-
-            boolean[] armiesRemaining = new boolean[players.size()];
-            boolean done = false;
-            while (!done)
-            {
-                for (int ii = 0; ii < players.size(); ii++)
-                {
-                    if (players.get(ii).playersLeft() > 0)
-                    {
-                        int random = (int) (Math.random() * players.get(ii).getOccupiedCountries().size());
-                        players.get(ii).getOccupiedCountries().get(random).setArmyCount(1);
-                    }
-                    else
-                    {
-                        armiesRemaining[ii] = true;
-                    }
-                }
-                int countP = 0;
-                for (boolean d : armiesRemaining)
-                {
-                    if (d)
-                    {
-                        countP++;
-                    }
-                }
-                if (countP == players.size())
-                {
-                    done = true;
-                }
-            }
 // FOR DEBUGGING PURPOSE -------------------------------------------------------------------------------------
-
 //            int totalArmy = 0;
             for (Player p : players)
             {
