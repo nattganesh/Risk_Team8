620a990df93927c8ce560884a46416ffbd20c8d9
AryaBaggins <tinjiklauglobal@gmail.com>
2019-02-02 16:50:12 -0500

    add dice last time by mistake

--- src/com/risk/dice/Dice.java
+++ src/com/risk/dice/Dice.java
@@ -11,7 +11,7 @@ package com.risk.dice;
  */
 public class Dice {
     
-    public static int roll(int num)
+    public static int roll()
     {
         return (int) (Math.random() * 6) + 1;
     }
