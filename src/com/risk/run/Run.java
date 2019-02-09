49e6b0a0d8d415da7161f26c8189c6f2ecf532dc
AryaBaggins <tinjiklauglobal@gmail.com>
2019-02-08 20:47:56 -0500

    Reduce if/else at attack phase by arya

--- src/com/risk/run/Run.java
+++ src/com/risk/run/Run.java
@@ -23,6 +23,7 @@ import java.util.logging.Logger;
 /**
  *
  * @author Natheepan
+ * @author Tianyi
  */
 public class Run {
 
@@ -387,6 +388,142 @@ public class Run {
         }
 //        System.out.println("\n---------------------------------------------");
     }
+    
+    public static void rollDice(int diceattack, int dicedefend, Country attack, Country defend) 
+    {
+		int[] dattack = new int[diceattack];
+		int[] ddefend = new int[dicedefend];
+		int rolltime;
+		for (int i = 0; i < diceattack; i++) 
+		{
+			dattack[i] = Dice.roll();
+		}
+		for (int i = 0; i < dicedefend; i++)
+		{
+			ddefend[i] = Dice.roll();
+		}
+		Arrays.sort(dattack);
+		Arrays.sort(ddefend);
+		if (diceattack >= dicedefend)
+		{
+			rolltime = dicedefend;
+		} else 
+		{
+			rolltime = diceattack;
+		}
+		for (int i = 0; i < rolltime; i++)
+		{
+			if (defend.getArmyCount() != 0) 
+			{
+				if (dattack[diceattack - 1 - i] > ddefend[dicedefend - 1 - i]) 
+				{
+					defend.reduceArmyCount();
+				} else 
+				{
+					attack.reduceArmyCount();
+				}
+			} else 
+			{
+				System.out.println("You have already occupied this country!");
+				break;
+			}
+		}
+	}
+	
+	public static int[] setRollLimt(Country attack, Country defend)
+	{
+		int dicerange_attack;
+		int dicerange_defend;
+		int[] result = new int[2];
+		if ((attack.getArmyCount() - 1) > 3) 
+		{
+			dicerange_attack = 3;
+		} else 
+		{
+			dicerange_attack = attack.getArmyCount() - 1;
+		}
+		if (defend.getArmyCount() >= 2) 
+		{
+			dicerange_defend = 2;
+		} else 
+		{
+			dicerange_defend = 1;
+		}
+		result[0] = dicerange_attack;
+		result[1] = dicerange_defend;
+		return result;
+	}
+	
+	public static Country isCountryBelongedtoPlayer(String c, Player p) 
+	{
+		String countryNeedtoCheck = c;
+		Country result = null;
+		for (Country country : p.getOccupiedCountries()) 
+		{
+			if (country.getName().equalsIgnoreCase(countryNeedtoCheck)) 
+			{
+				result = country;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	public static Country isCountryConnectedtoAttacker(String c, Country attack) 
+	{
+		String countryNeedtoCheck = c;
+		Country result = null;
+		for (Country country : attack.getConnectedCountries()) 
+		{
+			if (country.getName().equalsIgnoreCase(countryNeedtoCheck)) 
+			{
+				result=country;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	
+	public static String[] isAttackerCorrect(String c, Player p) 
+	{
+		String countryNeedtoCheck = c;
+		String[] result = new String[2];
+		Country attack = isCountryBelongedtoPlayer(countryNeedtoCheck, p);
+		if (attack == null) 
+		{
+			result[0] = "0";
+			result[1] = "This country does not belong to you! Please input again!";
+		} else if (attack.getArmyCount() < 2) 
+		{
+			result[0] = "0";
+			result[1] = "You don't have enough armies in this country! Please input again!";
+		}
+		result[0] = "1";
+		result[1] = "The attacker is determined!";
+		return result;
+	}
+	
+	public static String[] isDefenderCorrect(String c, Country attack) 
+	{
+		String countryNeedtoCheck = c;
+		Player p = attack.getRuler();
+		String[] result = new String[2];
+		if (isCountryBelongedtoPlayer(countryNeedtoCheck, p) != null) 
+		{
+			result[0] = "0";
+			result[1] = "You can't attack your own country! Please input again!";
+		} else if (isCountryConnectedtoAttacker(countryNeedtoCheck, attack)==null) 
+		{
+			result[0] = "0";
+			result[1] = "The defender you input is not connected to your attacker. Please try again!";
+		} else 
+		{
+			result[0] = "1";
+			result[1] = "The attacker and the defender are determined!";
+		}
+		return result;
+	}
 
     public static void main(String[] args)
     {
@@ -502,214 +639,109 @@ public class Run {
                     int diceattack;
                     int dicedefend;
                     int rolltime;
-                    while (true)
-                    {
-                        System.out.println("Do you want to invoke an attak? Y/N");
-                        String answer = key.nextLine();
-                        if (answer.equalsIgnoreCase("Y"))
-                        {
-                            while (true)
-                            {
-                                for (Country c : p.getOccupiedCountries())
-                                {
-                                    System.out.println(c.getArmyCount());
-                                }
-                                System.out.println("Please input the name of the attacker:");
-                                attacker = key.nextLine();
-                                for (Country c : p.getOccupiedCountries())
-                                {
-                                    if (c.getName().equalsIgnoreCase(attacker))
-                                    {
-                                        attack = c;
-                                        break;
-                                    }
-                                }
-                                if (attack == null)
-                                {
-                                    System.out.println("This country does not belong to you! Please input again!");
-                                    continue;
-                                }
-                                else
-                                {
-                                    if (attack.getArmyCount() < 2)
-                                    {
-                                        System.out.println(
-                                                "You don't have enough armies in this country! Please input again!");
-                                        continue;
-                                    }
-                                    else
-                                    {
-                                        while (true)
-                                        {
-                                            System.out.println("Please input the name of the defender:");
-                                            defender = key.nextLine();
-                                            Country tmp = null;
-                                            for (Country c : p.getOccupiedCountries())
-                                            {
-                                                if (c.getName().equalsIgnoreCase(defender))
-                                                {
-                                                    tmp = c;
-                                                    break;
-                                                }
-                                            }
-                                            if (tmp != null)
-                                            {
-                                                System.out.println(
-                                                        "You can't attack your own country! Please input again!");
-                                                continue;
-                                            }
-                                            else
-                                            {
-                                                for (Country c : attack.getConnectedCountries())
-                                                {
-                                                    System.out.println(c.getName());
-                                                }
+                    while (true) 
+					{
+						System.out.println("Do you want to invoke an attak? Y/N");
+						String answer = key.nextLine();
+						if (answer.equalsIgnoreCase("Y")) 
+						{
+							while (true) 
+							{
+								System.out.println("Please input the name of the attacker:");
+								attacker = key.nextLine();
+								String[] attackCheck = isAttackerCorrect(attacker, p);
+								System.out.println(attackCheck[1]);
+								if (attackCheck[0]=="0") 
+								{
+									continue;
+								} else 
+								{	
+									attack = isCountryBelongedtoPlayer(attacker, p);
+									while (true) 
+									{
+										System.out.println("These are countries connected to the attacker:");
+										for (Country c : attack.getConnectedCountries()) 
+										{
+											System.out.println(c.getName());
+										}
+										System.out.println("Please input the name of the defender:");
+										defender = key.nextLine();
+										String[] defenderCheck = isDefenderCorrect(defender, attack);
+										System.out.println(defenderCheck[1]);
+										if (defenderCheck[0].equals("0")) 
+										{
+											continue;
+										} else 
+										{
+											defend = isCountryConnectedtoAttacker(defender, attack);
+											break;
+										}
+									}
+									break;
 
-                                                for (Country c : attack.getConnectedCountries())
-                                                {
-                                                    if (c.getName().equalsIgnoreCase(defender))
-                                                    {
-                                                        defend = c;
-                                                        break;
-                                                    }
-                                                }
-                                                if (defend == null)
-                                                {
-                                                    System.out.println(
-                                                            "The defender you input is not connected to your attacker. Please try again!");
-                                                    continue;
-                                                }
-                                                else
-                                                {
-                                                    System.out.println("The attacker and the defender are determined!");
-                                                    break;
-                                                }
-                                            }
+								}
+							}
 
-                                        }
-                                        break;
-                                    }
-                                }
-                            }
+							int result[] = setRollLimt(attack, defend);
+							dicerange_attack = result[0];
+							dicerange_defend = result[1];
 
-                            if ((attack.getArmyCount() - 1) > 3)
-                            {
-                                dicerange_attack = 3;
-                            }
-                            else
-                            {
-                                dicerange_attack = attack.getArmyCount() - 1;
-                            }
-                            if (defend.getArmyCount() >= 2)
-                            {
-                                dicerange_defend = 2;
-                            }
-                            else
-                            {
-                                dicerange_defend = 1;
-                            }
+							while (true) 
+							{
+								System.out.println("Attacker army:" + attack.getArmyCount());
+								System.out.println("defender army:" + defend.getArmyCount());
+								System.out.println(
+										"Attacker "+attack.getRuler().getName()+ ": Please input the number of dice you want to roll. It should be at most "
+												+ dicerange_attack + " :");
+								diceattack = key.nextInt();
+								String clear = key.nextLine();
+								if (diceattack > dicerange_attack) 
+								{
+									System.out.println("The number you input is unavailable. Please input again!");
+									continue;
+								} else 
+								{
+									System.out.println("Defender " + defend.getRuler().getName()
+											+ ": Please input the number of dice you want to roll. It should be at most "
+											+ dicerange_defend + " :");
+									dicedefend = key.nextInt();
+									String clear1 = key.nextLine();
+									if (dicedefend > dicerange_defend) 
+									{
+										System.out.println("The number you input is unavailable. Please input again!");
+										continue;
+									} 
+									rollDice(diceattack, dicedefend, attack, defend);
+									System.out.println("Attacker army:" + attack.getArmyCount());
+									System.out.println("defender army:" + defend.getArmyCount());
+									if (defend.getArmyCount() == 0) 
+									{
+										defend.setRuler(p);
+										System.out.println(defend.getRuler().getName());
+										System.out.println("You have already occupied this country!");
+										break;
+									} else 
+									{
+										System.out.println("Do you want to continue attacking this country? Y/N");
+										String continueornot;
+										continueornot = key.next();
+										if (continueornot.equalsIgnoreCase("Y")) 
+										{
+											continue;
+										} else 
+										{
+											break;
+										}
+									}
 
-                            while (true)
-                            {
-                                System.out.println(
-                                        "Attacker: Please input the number of dice you want to roll. It should be at most "
-                                        + dicerange_attack + " :");
-                                diceattack = key.nextInt();
-                                String clear = key.nextLine();
-                                if (diceattack > dicerange_attack)
-                                {
-                                    System.out.println("The number you input is unavailable. Please input again!");
-                                    continue;
-                                }
-                                else
-                                {
-                                    System.out.println("Attacker army:" + attack.getArmyCount());
-                                    System.out.println("defender army:" + defend.getArmyCount());
-                                    System.out.println("Defender " + defend.getRuler().getName()
-                                            + ": Please input the number of dice you want to roll. It should be at most "
-                                            + dicerange_defend + " :");
-                                    dicedefend = key.nextInt();
-                                    String clear1 = key.nextLine();
-                                    if (dicedefend > dicerange_defend)
-                                    {
-                                        System.out.println("The number you input is unavailable. Please input again!");
-                                        continue;
-                                    }
-                                    else
-                                    {
-                                        int[] dattack = new int[diceattack];
-                                        int[] ddefend = new int[dicedefend];
-                                        for (int i = 0; i < diceattack; i++)
-                                        {
-                                            dattack[i] = Dice.roll();
-                                        }
-                                        for (int i = 0; i < dicedefend; i++)
-                                        {
-                                            ddefend[i] = Dice.roll();
-                                        }
-                                        Arrays.sort(dattack);
-                                        Arrays.sort(ddefend);
-                                        if (diceattack >= dicedefend)
-                                        {
-                                            rolltime = dicedefend;
-                                        }
-                                        else
-                                        {
-                                            rolltime = diceattack;
-                                        }
-                                        for (int i = 0; i < rolltime; i++)
-                                        {
-                                            if (defend.getArmyCount() != 0)
-                                            {
-                                                if (dattack[diceattack - 1 - i] > ddefend[dicedefend - 1 - i])
-                                                {
-                                                    defend.reduceArmyCount();
-                                                }
-                                                else
-                                                {
-                                                    attack.reduceArmyCount();
-                                                }
-                                            }
-                                            else
-                                            {
-                                                System.out.println("You have already occupied this country!");
-                                                break;
-                                            }
-                                        }
-                                        System.out.println("Attacker army:" + attack.getArmyCount());
-                                        System.out.println("defender army:" + defend.getArmyCount());
-                                        if (defend.getArmyCount() == 0)
-                                        {
-                                            defend.setRuler(p);
-                                            System.out.println(defend.getRuler().getName());
-                                            System.out.println("You have already occupied this country!");
-                                            break;
-                                        }
-                                        else
-                                        {
-                                            System.out.println("Do you want to continue attacking this country? Y/N");
-                                            String continueornot;
-                                            continueornot = key.next();
-                                            if (continueornot.equalsIgnoreCase("Y"))
-                                            {
-                                                continue;
-                                            }
-                                            else
-                                            {
-                                                break;
-                                            }
-                                        }
+								}
+							}
+						} else 
+						{
+							break;
+						}
 
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
-
-                    }
+					}
 
                     System.out.println("\n---------------------------------------------");
                     System.out.println("Time To Fortification");
