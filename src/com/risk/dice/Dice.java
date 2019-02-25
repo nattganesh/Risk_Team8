beb934fd484b601875e7e592c713bdccea328b42
AryaBaggins <tinjiklauglobal@gmail.com>
2019-02-24 20:47:11 -0500

    move a method to dice

--- src/com/risk/dice/Dice.java
+++ src/com/risk/dice/Dice.java
@@ -8,17 +8,48 @@ package com.risk.dice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import com.risk.map.Country;;
 
-/**
- * This method calculates the random number between 1-6 representing the dice roll
- * 
- * @return number of the dice roll between 1-6
- */
+
 public class Dice {
-
+	/**
+	 * This method calculates the random number between 1-6 representing the dice roll
+	 * 
+	 * @return number of the dice roll between 1-6
+	 */
     public static int roll()
     {
         return (int) (Math.random() * 6) + 1;
     }
 
+    /**
+   	 * This method gets the maximum of number of dices the attacker and defender can roll
+   	 * 
+  	 * @param attack The country which invokes the attack
+   	 * @param defend The country which is attacked
+   	 * @return The result corresponding to the number of armies of attacker and defender
+   	 */
+    public static int[] setRollLimt(Country attack, Country defend)
+	{
+		int dicerange_attack;
+		int dicerange_defend;
+		int[] result = new int[2];
+		if ((attack.getArmyCount() - 1) > 3) 
+		{
+			dicerange_attack = 3;
+		} else 
+		{
+			dicerange_attack = attack.getArmyCount() - 1;
+		}
+		if (defend.getArmyCount() >= 2) 
+		{
+			dicerange_defend = 2;
+		} else 
+		{
+			dicerange_defend = 1;
+		}
+		result[0] = dicerange_attack;
+		result[1] = dicerange_defend;
+		return result;
+	}
 }
