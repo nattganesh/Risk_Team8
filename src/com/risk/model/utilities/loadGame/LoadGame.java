59c0f13ac6ad633029b23fd0413a2b61b9a454fd
Natheepan Ganeshamoorthy <nattganesh@outlook.com>
2019-03-27 20:48:59 -0400

    Load Game class Template

---
+++ src/com/risk/model/utilities/loadGame/LoadGame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.risk.model.utilities.loadGame;
+
+/**
+ * This class is necessary for loading a game from a file
+ *
+ * @author Natheepan
+ */
+public class LoadGame {
+
+    /**
+     * This is a constructor for the Output class
+     */
+    private LoadGame()
+    {
+    }
+
+}
