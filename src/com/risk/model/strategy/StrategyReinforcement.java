69ec781d228b76736ceea2f339d577c3a6d05ba6
Natheepan Ganeshamoorthy <nattganesh@outlook.com>
2019-03-31 18:26:16 -0400

    Strategy pattern interfaces

---
+++ src/com/risk/model/strategy/StrategyReinforcement.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.risk.model.strategy;
+
+/**
+ *
+ * @author Natheepan
+ */
+public interface StrategyReinforcement {
+
+    public void reinforce(int Armyinput);
+
+}
