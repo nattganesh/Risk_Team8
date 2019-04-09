aa3ed5e3df2d76c604b45b7bf4619793f9fd8cdb
AryaBaggins <tinjiklauglobal@gmail.com>
2019-04-08 23:04:20 -0400

    add junit tests

--- test/com/risk/model/strategy/BenevolentTest.java
+++ test/com/risk/model/strategy/BenevolentTest.java
@@ -7,6 +7,8 @@ import com.risk.model.MapModel;
 import com.risk.model.card.Card;
 import com.risk.model.map.Continent;
 import com.risk.model.map.Country;
+import com.risk.model.player.Player;
+
 import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
@@ -25,7 +27,14 @@ import static org.junit.Assert.*;
  * @author Tianyi
  */
 public class BenevolentTest {
-	
+	private Player p1;
+	private Player p2;
+	private Country c1;
+	private Country c2;
+	private Country c3;
+	private ArrayList<Country> occupiedCountries1 = new ArrayList<Country>();
+	private ArrayList<Country> occupiedCountries2 = new ArrayList<Country>();
+	private Continent continent;
 	
     public BenevolentTest()
     {
@@ -34,6 +43,25 @@ public class BenevolentTest {
     @Before
     public void setUp()
     {
+    	continent = new Continent("Asia",0);
+    	p1 = new Player("Green");
+    	p1.setStrategy(new Benevolent());
+    	p2 = new Player("Red");
+    	p2.setStrategy(new Benevolent());
+    	c1 = new Country("China");
+		c2 = new Country("Quebec");
+		c3 = new Country("Siam");
+		
+		continent.setCountry(c1);
+    	continent.setCountry(c2);
+    	continent.setCountry(c3);
+    	c1.setContinent(continent);
+    	c2.setContinent(continent);
+    	c3.setContinent(continent);
+		c1.getConnectedCountries().add(c2);
+		c1.getConnectedCountries().add(c3);
+		c2.getConnectedCountries().add(c1);
+		c3.getConnectedCountries().add(c1);
     }
     
     @After
@@ -47,15 +75,148 @@ public class BenevolentTest {
     	
     }
     
-    @Test public void testReinforce()
+    @Test public void testReinforce1()
     {
-    	
+		c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		c1.setIsOccupied(true);
+		c2.setIsOccupied(true);
+		c3.setIsOccupied(true);
+    	occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		p1.setOccupiedCountries(occupiedCountries1);
+    	c1.setArmyCount(1);
+    	c2.setArmyCount(1);
+		c3.setArmyCount(1);
+    	p1.reinforceStrategy(p1);
+    	int expectResult = 2;
+    	int result1 = c1.getArmyCount();
+    	int result2 = c2.getArmyCount();
+    	int result3 = c3.getArmyCount();
+    	assertEquals(expectResult, result1);
+    	assertEquals(expectResult, result2);
+    	assertEquals(expectResult, result3);
+    }
+    
+    @Test public void testReinforce2()
+    {
+		c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		c1.setIsOccupied(true);
+		c2.setIsOccupied(true);
+		c3.setIsOccupied(true);
+    	occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		p1.setOccupiedCountries(occupiedCountries1);
+    	c1.setArmyCount(1);
+    	c2.setArmyCount(3);
+		c3.setArmyCount(4);
+    	p1.reinforceStrategy(p1);
+    	int expectResult = 4;
+    	int result1 = c1.getArmyCount();
+    	assertEquals(expectResult, result1);
     }
     
     @Test public void testFortify()
     {
-    	
+    	c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		c1.setIsOccupied(true);
+		c2.setIsOccupied(true);
+		c3.setIsOccupied(true);
+    	occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		p1.setOccupiedCountries(occupiedCountries1);
+    	c1.setArmyCount(1);
+    	c2.setArmyCount(3);
+		c3.setArmyCount(4);
+    	p1.fortifyStrategy(p1);
+    	int expectResult1 = 4;
+    	int expectResult2 = 1;
+    	int result1 = c1.getArmyCount();
+    	int result2 = c3.getArmyCount();
+    	assertEquals(expectResult1, result1);
+    	assertEquals(expectResult2, result2);
+    }
+    
+    @Test public void testFortify1()
+    {
+    	c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		c1.setIsOccupied(true);
+		c2.setIsOccupied(true);
+		c3.setIsOccupied(true);
+    	occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		Country c4 = new Country("Korean");
+		Country c5 = new Country("Mongolia");
+		c5.getConnectedCountries().add(c4);
+		c4.getConnectedCountries().add(c5);
+		c4.setRuler(p1);
+		c5.setRuler(p1);
+		occupiedCountries1.add(c4);
+		occupiedCountries1.add(c5);
+		continent.setCountry(c4);
+		continent.setCountry(c5);
+    	c4.setContinent(continent);
+    	c5.setContinent(continent);
+    	c4.setIsOccupied(true);
+		c5.setIsOccupied(true);
+		p1.setOccupiedCountries(occupiedCountries1);
+    	c1.setArmyCount(1);
+    	c2.setArmyCount(1);
+		c3.setArmyCount(1);
+		c4.setArmyCount(3);
+		c5.setArmyCount(3);
+    	p1.fortifyStrategy(p1);
+    	int expectResult = 1;
+    	int result1 = c1.getArmyCount();
+    	int result2 = c2.getArmyCount();
+    	int result3 = c3.getArmyCount();
+    	assertEquals(expectResult, result1);
+    	assertEquals(expectResult, result2);
+    	assertEquals(expectResult, result3);
     }
 
+    @Test
+    public void testSetup()
+    {
+    	c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		c1.setIsOccupied(true);
+		c2.setIsOccupied(true);
+		c3.setIsOccupied(true);
+    	occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+    	Country c4 = new Country("Korean");
+    	c1.getConnectedCountries().add(c4);
+		c4.getConnectedCountries().add(c1);
+		c4.setRuler(p1);
+		occupiedCountries1.add(c4);
+		continent.setCountry(c4);
+    	c4.setContinent(continent);
+		c4.setIsOccupied(true);
+		p1.setOccupiedCountries(occupiedCountries1);
+    	p1.setStartingPoints(12);
+    	p1.setupStrategy(p1);
+    	int expectResult1 = 3;
+    	p1.setupStrategy(p1);
+    	int result1 = p1.getStartingPoints();
+    	assertEquals(0,result1);
+    	assertEquals(expectResult1, c1.getArmyCount());
+    	assertEquals(expectResult1, c2.getArmyCount());
+    	assertEquals(expectResult1, c3.getArmyCount());
+    	assertEquals(expectResult1, c4.getArmyCount());
+    }
  
 }
--- test/com/risk/model/strategy/RandomTest.java
+++ test/com/risk/model/strategy/RandomTest.java
@@ -7,7 +7,10 @@ import com.risk.model.MapModel;
 import com.risk.model.card.Card;
 import com.risk.model.map.Continent;
 import com.risk.model.map.Country;
+import com.risk.model.player.Player;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -25,10 +28,35 @@ import static org.junit.Assert.*;
  * @author Tianyi
  */
 public class RandomTest {
-	
+	private Player p1;
+	private Player p2;
+	private Country c1;
+	private Country c2;
+	private Country c3;
+	private ArrayList<Country> occupiedCountries1 = new ArrayList<Country>();
+	private ArrayList<Country> occupiedCountries2 = new ArrayList<Country>();
+	private Continent continent;
 	
     public RandomTest()
     {
+    	continent = new Continent("Asia",0);
+    	p1 = new Player("Green");
+    	p1.setStrategy(new Random());
+    	p2 = new Player("Red");
+    	p2.setStrategy(new Benevolent());
+    	c1 = new Country("China");
+		c2 = new Country("Quebec");
+		c3 = new Country("Siam");
+		continent.setCountry(c1);
+    	continent.setCountry(c2);
+    	continent.setCountry(c3);
+    	c1.setContinent(continent);
+    	c2.setContinent(continent);
+    	c3.setContinent(continent);
+		c1.getConnectedCountries().add(c2);
+		c1.getConnectedCountries().add(c3);
+		c2.getConnectedCountries().add(c1);
+		c3.getConnectedCountries().add(c1);
     }
     
     @Before
@@ -44,18 +72,122 @@ public class RandomTest {
     
     @Test public void testAttack()
     {
-    	
+    	c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		p1.setOccupiedCountries(occupiedCountries1);
+		c1.setArmyCount(20);
+    	c2.setArmyCount(20);
+		c3.setArmyCount(20);
+		Country c4 = new Country("Korean");
+    	c1.getConnectedCountries().add(c4);
+		c4.getConnectedCountries().add(c1);
+		c2.getConnectedCountries().add(c4);
+		c4.getConnectedCountries().add(c2);
+		c3.getConnectedCountries().add(c4);
+		c4.getConnectedCountries().add(c3);
+		c4.setRuler(p2);
+		occupiedCountries2.add(c4);
+		p2.setOccupiedCountries(occupiedCountries2);
+		c4.setArmyCount(2);
+		int expectResult = c1.getArmyCount()+c2.getArmyCount()+c3.getArmyCount();
+		HashMap<String,Integer> countriesBeforeFortify = new HashMap<String,Integer>();
+		for(Country c: p1.getOccupiedCountries()) 
+		{
+			countriesBeforeFortify.put(c.getName(), c.getArmyCount());
+		}
+		p1.attackStrategy(p1);
+		int result = c1.getArmyCount()+c2.getArmyCount()+c3.getArmyCount();
+		HashMap<String,Integer> countriesAfterFortify = new HashMap<String,Integer>();
+		for(Country c: p1.getOccupiedCountries()) 
+		{
+			countriesAfterFortify.put(c.getName(), c.getArmyCount());
+		}
+		assertTrue(expectResult!=result);
+		boolean different = false;
+		for(String c: countriesAfterFortify.keySet()) 
+		{
+			if(countriesAfterFortify.get(c)!=countriesBeforeFortify.get(c)||!countriesBeforeFortify.containsKey(c))
+			{
+				different = true;
+			}
+		}
+		assertTrue(different);
     }
     
     @Test public void testReinforce()
     {
-    	
+    	c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		p1.setOccupiedCountries(occupiedCountries1);
+		c1.setArmyCount(10);
+    	c2.setArmyCount(2);
+		c3.setArmyCount(2);
+		int expectResult1 = 14;
+		p1.reinforceStrategy(p1);
+		
+		int result = c1.getArmyCount()+ c2.getArmyCount()+c3.getArmyCount();
+		assertTrue(result>expectResult1);
     }
     
     @Test public void testFortify()
     {
-    	
+    	c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		p1.setOccupiedCountries(occupiedCountries1);
+		c1.setArmyCount(10);
+    	c2.setArmyCount(2);
+		c3.setArmyCount(2);
+		int expectResult = c1.getArmyCount()+c2.getArmyCount()+c3.getArmyCount();
+		HashMap<String,Integer> countriesBeforeFortify = new HashMap<String,Integer>();
+		for(Country c: p1.getOccupiedCountries()) 
+		{
+			countriesBeforeFortify.put(c.getName(), c.getArmyCount());
+		}
+		p1.fortifyStrategy(p1);
+		int result = c1.getArmyCount()+c2.getArmyCount()+c3.getArmyCount();
+		HashMap<String,Integer> countriesAfterFortify = new HashMap<String,Integer>();
+		for(Country c: p1.getOccupiedCountries()) 
+		{
+			countriesAfterFortify.put(c.getName(), c.getArmyCount());
+		}
+		assertEquals(expectResult, result);
+		boolean different = false;
+		for(String c: countriesBeforeFortify.keySet()) 
+		{
+			if(countriesBeforeFortify.get(c)!=countriesAfterFortify.get(c))
+			{
+				different = true;
+			}
+		}
+		assertTrue(different);
     }
 
+    @Test
+    public void testSetup() {
+    	c1.setRuler(p1);
+		c2.setRuler(p1);
+		c3.setRuler(p1);
+		occupiedCountries1.add(c1);
+		occupiedCountries1.add(c2);
+		occupiedCountries1.add(c3);
+		p1.setOccupiedCountries(occupiedCountries1);
+		p1.setStartingPoints(10);
+		p1.setupStrategy(p1);
+		int expectResult1 = 10;
+		int result = c1.getArmyCount()+ c2.getArmyCount()+c3.getArmyCount();
+		assertEquals(expectResult1, result);
+    }
  
 }
